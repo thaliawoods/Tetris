1eb466d0eb877f8136caa9b7e183cf5ef84e4c9a
thaliawoods <thaliadelano@gmail.com>
2024-02-21 18:17:05 +0100

    Features Rotate/Down

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const ScoreDisplay = document.querySelector('#score')
     const StartBtn = document.querySelector('#start-button')
     const width = 10
+    let nextRandom = 0
 
 
   //create the Tetrominoes
@@ -84,10 +85,10 @@ function control(e) {
     if(e.keyCode === 37) {
         moveLeft()
     } else if (e.keyCode === 38) {
-        // rotate()
+        rotate()
     } else if (e.keyCode === 39) {
         moveRight()
-    } else if (e.keyCode === 40) {
+    } else if (e.keyCode === 68) {
         moveDown()
     }
 }
@@ -109,6 +110,7 @@ function freeze() {
     if(current.some(index=>squares[currentPosition + index + width].classList.contains('taken'))) {
         current.forEach(index => squares[currentPosition + index].classList.add('taken'))
     // start new tetromino fall
+    random = nextRandom
     random = Math.floor(Math.random() * theTetrominoes.length)
     current = theTetrominoes[random][currentRotation]
     currentPosition = 4
@@ -134,7 +136,7 @@ function moveLeft() {
 
 // move tetromino right, unless it is at the edge or if there is a blockage
 function moveRight() {
-    undraw()
+    undraw() 
     const isAtRightEdge = current.some(index => (currentPosition + index) % width === -1)
 
     if(!isAtRightEdge) currentPosition +=1
@@ -144,6 +146,44 @@ function moveRight() {
     }
 
     draw()
+}
+
+// rotate the tetromino
+function rotate () {
+    undraw()
+    currentRotation ++
+    // if the current rotation gets to 4, make it go back to 0
+    if(currentRotation === current.length) {
+        currentRotation = 0
+    }
+    current = theTetrominoes[random][currentRotation]
+    draw()
+}
+
+// show up-next tetromino in mini-grid
+const displaySquares = document.querySelector('.mini-grid div')
+const displayWidth = 4
+let displayIndex = 0
+
+
+// the Tetrominos without rotations
+const upNextTetrominoes = [
+    [1, displayWidth+1, displayWidth*2+1, 2], // lTetromino
+    [0, displayWidth, displayWidth+1, displayWidth*2+1], //zTetromino
+    [1, displayWidth, displayWidth+1, displayWidth*2], //tTetromino
+    [0, 1, displayWidth, displayWidth+1], //oTetromino
+    [1, displayWidth+1, displayWidth*2+1, displayWidth*3+1], //iTetromino
+]
+
+// display the shape in the mini-grid display
+function displayShape() {
+    // remove any trace of a tetromino from the entire grid
+    displaySquares.forEach(square => {
+        square.classList.remove('tetromino')
+    })
+    upNextTetrominoes[nextRandom].forEach( index => {
+        displaySquares[displayIndex + index].classList.add('tetromino')
+    })
 }
 
 })
