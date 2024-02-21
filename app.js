f0b2c03750633db9872bce8d036ccc8416858914
thaliawoods <thaliadelano@gmail.com>
2024-02-21 12:03:47 +0100

    Freeze

--- app.js
+++ app.js
@@ -70,4 +70,29 @@ function undraw() {
     })
 }
 
+
+// move down every second
+timerId = setInterval(moveDown, 1000)
+
+// move down function
+function moveDown() {
+    undraw()
+    currentPosition += width
+    draw()
+    freeze()
+}
+
+// freeze function
+function freeze() {
+    if(current.some(index=>squares[currentPosition + index + width].classList.contains('taken'))) {
+        current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+    // start new tetromino fall
+    random = Math.floor(Math.random() * theTetrominoes.length)
+    current = theTetrominoes[random][currentRotation]
+    currentPosition = 4
+    draw()
+    }
+}
+
+
 })
